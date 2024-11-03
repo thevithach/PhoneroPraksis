4a08069320fbd4754fe1fac182aad3113ebd0410
thevithach <vitt@uia.no>
2024-11-03 16:47:42 +0100

    Remove unused import and update iframe attributes in Phonero_Status_2.tsx

--- src/Pages/Phonero_Status_2.tsx
+++ src/Pages/Phonero_Status_2.tsx
@@ -1,4 +1,3 @@
-import { text } from "stream/consumers";
 import phonero from "../assets/phoneroLogoSmall.png";
 import phoneroPhoneView from "../assets/PhoneroMobileView.png";
 
@@ -87,10 +86,10 @@ const Phonero_Status_2 = () => {
             height="360"
             src="https://www.youtube.com/embed/3CpYzzf5hEo?si=MbHRt1jVecvyhi5W"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
             style={{ float: "left" }}
           ></iframe>
         </div>
